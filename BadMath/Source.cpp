f6c3bf47ebd4fac955d434e1ee862f70c27e07f7
Akkeela <Akkeela.kaleelurrahman@artsphereakkeela.kaleelurrahman@artsphereinc.org>
2025-04-09 13:04:18 -0400

    Fix input handling and average calculation logic

--- BadMath/Source.cpp
+++ BadMath/Source.cpp
@@ -1,23 +1,23 @@
 #include <iostream>
-#include <iomanip>
 using namespace std;
 
-float average(int i1, int i2, int i3)
-{
-	return i1 + i2 + i3 / 3.0f;
-}
+int main() {
+    int num1, num2, num3;
 
-int main()
-{
-	int n1 =0 , n2 = 0, n3 = 0;
-	cout << "This program calculates the average of three numbers." << endl;
-	cout << "First number: "; cin >> n1;
-	cout << "Second number: "; cin >> n2;
-	cout << "Third number: "; cin >> n2;
+    cout << "This program calculates the average of three numbers." << endl;
 
-	float a = average(n1, n2, n3);
+    cout << "First number: ";
+    cin >> num1;
 
-	cout << setprecision(1) << fixed << "The average is " << a << endl;
+    cout << "Second number: ";
+    cin >> num2;
 
-	return 0;
+    cout << "Third number: ";
+    cin >> num3;
+
+    double average = (num1 + num2 + num3) / 3.0;
+
+    cout << "The average is " << average << endl;
+
+    return 0;
 }
